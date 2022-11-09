cc1e0331c58abdb1648207ee2393a3398f987503
serghei paraschevici <paraskevichi777@gmail.com>
2022-11-09 23:30:48 +0100

    add_routing

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -1,17 +1,25 @@
 import './App.css'
+import Dialogs from './components/Dialogs';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
+    <BrowserRouter>
     <div className="app_wrapper">
         <Header/>
         <Navbar/>
-        <Main/>
-     
-    
+        <div className='main'>
+       <Routes>
+          <Route path='/profile' element={<Main/>}/>  
+          <Route path='/messages' element={<Dialogs/>}/>  
+            
+      </Routes>
+        
+        </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- react_test/src/components/Navbar/Navbar.jsx
+++ react_test/src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import style from './style.module.css'
 export default function Navbar() {
   return (
     <nav className={style.navbar}>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
+        <div><a href='/profile'>Profile</a></div>
+        <div><a href='/messages'>Messages</a></div>
         <div><a>News</a></div>
         <div><a>Music</a></div>
         <div><a>Settings</a></div>
