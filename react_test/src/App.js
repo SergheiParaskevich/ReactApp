edf35911a417c61b0ad9a440c3ba90becad5be0a
serghei paraschevici <paraskevichi777@gmail.com>
2022-11-10 23:33:53 +0100

    add_nav_link

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -1,24 +1,28 @@
-import './App.css'
-import Dialogs from './components/Dialogs';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Messages from "./components/Messages";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import News from "./components/News";
+import Music from "./components/Music";
+import Options from "./components/Options";
 function App() {
   return (
     <BrowserRouter>
-    <div className="app_wrapper">
-        <Header/>
-        <Navbar/>
-        <div className='main'>
-       <Routes>
-          <Route path='/profile' element={<Main/>}/>  
-          <Route path='/messages' element={<Dialogs/>}/>  
-            
-      </Routes>
-        
+      <div className="app_wrapper">
+        <Header />
+        <Navbar />
+        <div className="main">
+          <Routes>
+            <Route path="/profile" element={<Main />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/options" element={<Options />} />
+          </Routes>
         </div>
-    </div>
+      </div>
     </BrowserRouter>
   );
 }
--- react_test/src/components/Navbar/Navbar.jsx
+++ react_test/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import style from './style.module.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import style from "./style.module.css";
 
 export default function Navbar() {
   return (
     <nav className={style.navbar}>
-        <div><a href='/profile'>Profile</a></div>
-        <div><a href='/messages'>Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
+      <div>
+        <NavLink to='/profile'>Profile</NavLink>
+      </div>
+      <div>
+       <NavLink to='/messages' className={style.active}>Messages</NavLink>
+      </div>
+      <div>
+        <NavLink to='/news'>News</NavLink>
+      </div>
+      <div>
+      <NavLink to='/music'>Music</NavLink>
+      </div>
+      <div>
+        <NavLink to='/options'>Options</NavLink>
+      </div>
     </nav>
-  )
+  );
 }
